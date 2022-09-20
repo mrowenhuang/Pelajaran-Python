501cf4ce1b9968561c00d51980038856392ae47f
MrOwenHuang <williamowenwijaya191204@gmail.com>
2022-09-20 11:32:49 +0700

    list Pembelajaran Python

---
+++ uji_coba.py
@@ -0,0 +1,7 @@
+#perkalian perngulangan 
+
+angka = int(input("masukan angka perkalian: "))
+jumlah = int(input("masukan angka jumlah kali : "))
+
+for i in range(jumlah+1):
+    print(f"{i} X {angka} = {i*angka}") 
